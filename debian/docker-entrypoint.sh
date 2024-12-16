143812e0177f36d0fabf2949eeaa223ee1e8ff03
snowdream <sn0wdr1am@qq.com>
2024-12-16 16:23:33 +0800

    feat: Generate random ssh root password

--- debian/docker-entrypoint.sh
+++ debian/docker-entrypoint.sh
@@ -9,8 +9,12 @@ if [ ! -f "/etc/dropbear/dropbear_rsa_host_key" ]; then
   dropbearkey -t ecdsa -s 521 -f /etc/dropbear/dropbear_ecdsa_host_key >/dev/null 2>&1
 fi
 
-# # fastfetch from https://github.com/fastfetch-cli/fastfetch
-# fastfetch --pipe false >/etc/motd 2>&1
+
+if [ -z "${SSH_ROOT_PASSWORD}" ]; then {
+    SSH_ROOT_PASSWORD=$(openssl rand -base64 33)
+    echo "Generate random ssh root password:${SSH_ROOT_PASSWORD}"
+}
+fi
 
 # change the password for root
 echo "root:$SSH_ROOT_PASSWORD" | chpasswd >/dev/null 2>&1
@@ -18,9 +22,9 @@ echo "root:$SSH_ROOT_PASSWORD" | chpasswd >/dev/null 2>&1
 # generate ssh keys
 if [ ! -d "$HOME/.ssh" ]; then
   mkdir -p $HOME/.ssh
-  dropbearkey -t ed25519 -C "user@example.com" -f $HOME/.ssh/id_ed25519 >/dev/null 2>&1
-  dropbearkey -t rsa -s 4096 -C "user@example.com" -f $HOME/.ssh/id_rsa >/dev/null 2>&1
-  dropbearkey -t ecdsa -s 521 -C "user@example.com" -f $HOME/.ssh/id_ecdsa >/dev/null 2>&1
+  dropbearkey -t ed25519  -f $HOME/.ssh/id_ed25519 >/dev/null 2>&1
+  dropbearkey -t rsa -s 4096  -f $HOME/.ssh/id_rsa >/dev/null 2>&1
+  dropbearkey -t ecdsa -s 521  -f $HOME/.ssh/id_ecdsa >/dev/null 2>&1
 fi
 
 # start dropbear
